11fdc52ff60e3f49d8529f632d3010da3e192eb5
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2026-04-16 20:15:00 -0300

    config: estrutura inicial do projeto Next.js

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+import withSerwistInit from "@serwist/next";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV !== "production",
+});
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  reactCompiler: true,
+  productionBrowserSourceMaps: true,
+  allowedDevOrigins: ["192.168.1.172"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+export default withSerwist(withNextIntl(nextConfig));
